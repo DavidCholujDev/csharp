de9e6c6ec24bc17f083e5d839496b9de5497b56b
David Choluj <davidcholuj12@gmail.com>
2025-09-24 09:25:42 +0200

    trochu som skusal

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿float neviem = 0.11111f;
+int mojeCislo = 123456;
+string mojeSlovo = "Hello World";
+bool mojaBoolHodnota = true;
+DateTime dnesnyDatum = DateTime.Now;
+var neviem2 = "dfghjk";
+int sucet = mojeCislo + 5;
+
+Console.WriteLine(mojeCislo);
+Console.WriteLine(mojeSlovo);
+Console.WriteLine(dnesnyDatum.ToString("yyyy.MM.dd"));
+Console.WriteLine(mojaBoolHodnota);
+Console.WriteLine(sucet);
+Console.WriteLine($"Hodnota Float je:{neviem}");
+Console.WriteLine(neviem2);
