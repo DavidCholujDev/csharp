0be28cdcc80cd2dad5745d01ae4f345031256a6b
David Choluj <davidcholuj12@gmail.com>
2025-09-24 10:44:09 +0200

    Zfgh

--- src/AppsLab-006-Constants/Program.cs
+++ src/AppsLab-006-Constants/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const int MaxPoints = 100;
+const int MinAgeForDrivingLicence = 17;
+const string WelcomeMessage = "Vitajte tu v tomto teste";
+
+Console.WriteLine(MaxPoints);
+Console.WriteLine(MinAgeForDrivingLicence);
+Console.WriteLine(WelcomeMessage);
