1a5f779c3c1319cf6f05394bff8b0b1239985b4e
David Choluj <davidcholuj12@gmail.com>
2025-10-08 09:41:00 +0200

    moja nova kalkulacka so swithconm

--- homework/Program.cs
+++ homework/Program.cs
@@ -1,39 +1,38 @@
 ﻿Console.WriteLine("Zadaj cislo 1");
 string number1 = Console.ReadLine();
 decimal cislo1 = decimal.Parse(number1);
-Console.WriteLine("Zadaj operaciu");
+Console.WriteLine("Zadaj operaciu +-/%");
 string operacia = Console.ReadLine();
 Console.WriteLine("Zadaj cislo 2");
 string number2 = Console.ReadLine();
 decimal cislo2 = decimal.Parse(number2);
 
-if (operacia == "+")
+decimal vysledok = 0;
+switch (operacia)
 {
-    decimal vysledok = (cislo1 + cislo2);
-    Console.WriteLine($"vysledok je {vysledok}");
+    case "+":
+        vysledok = cislo1 + cislo2;
+        Console.WriteLine(vysledok);
+        break;
+    case "-":
+        vysledok = cislo2 - cislo2;
+        Console.WriteLine(vysledok);
+        break;
+    case "/":
+        if (cislo2 == 0) 
+        {
+            Console.WriteLine("ty magor nulou sa neda delit");
+        }
+        vysledok = cislo1 / cislo2;     
+        Console.WriteLine(vysledok);
+        break;
+    case "*":
+        vysledok = cislo1 * cislo2;
+        Console.WriteLine(vysledok);
+        break;
+    case "%":
+        vysledok = (cislo1 / cislo2);
+        Console.WriteLine(vysledok);
+        break;
 }
 
-else if (operacia == "-")
-{
-    decimal vysledok = (cislo1 - cislo2);
-    Console.WriteLine($"vysledok je {vysledok}");
-}
-
-else if (operacia == "*")
-{
-    decimal vysledok =(cislo1 * cislo2);
-    Console.WriteLine($"vysledok je {vysledok}");
-}
-
-else if(operacia == "/")
-{
-    decimal vysledok =(cislo1 / cislo2);
-    Console.WriteLine($"vysledok je {vysledok}");
-}
-
-else if  (operacia == "%")
-{
-    decimal vysledok = (cislo1 % cislo2);
-    Console.WriteLine($"zvysek je {vysledok}");
-}
-Console.WriteLine("dakujem ze si pouzil moju kalkulacku, dovidenia");
